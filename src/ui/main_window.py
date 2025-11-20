c1f64556e0297dffb2d3bc4c3b22d490eccabe74
evanwiseman <evanwiseman2000@gmail.com>
2025-11-20 14:14:50 -0800

    clean main window

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -38,122 +38,59 @@ class MainWindow(QMainWindow):
         self._mqtt_service = mqtt_service
         self.health_service = health_service
 
+        self.init_menu()
+        self.init_status()
+        self.init_tool()
         self.init_ui()
 
         self._mqtt_service.message_signal.connect(self.handle_message)
 
+    def init_menu(self):
+        self.menu = self.menuBar()
+
+        self.file_menu = self.menu.addMenu("File")
+        exit_action = self.file_menu.addAction("Exit")
+        exit_action.triggered.connect(self.close)
+
+        self.view_menu = self.menu.addMenu("View")
+        self._view_actions: dict[str, QAction] = {}
+
+    def init_status(self):
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+
+    def init_tool(self):
+        self.tool = QToolBar()
+        self.tool.setObjectName("mqttToolBar")
+        self.addToolBar(self.tool)
+
     def init_ui(self):
+        self._init_document_area()
+
+        self._init_monitors()
+        self._init_heartbeats()
+        self._init_wdlms()
+        self._init_mqtt()
+
+        self.setCentralWidget(self.document_tabs)
+
+    # ========================
+    # Document Tabs
+    # ========================
+
+    def _init_document_area(self) -> None:
         # Central document area
         self.document_tabs = QTabWidget()
         self.document_tabs.setTabsClosable(True)
         self.document_tabs.setMovable(True)
         self.document_tabs.setDocumentMode(True)
         self.document_tabs.tabCloseRequested.connect(self.close_tab)
-        self.setCentralWidget(self.document_tabs)
 
-        # Create menubar for document and dock control
-        menu_bar = self.menuBar()
-
-        view_menu = menu_bar.addMenu("View")
-        self._view_actions: dict[str, QAction] = {}
-
-        file_menu = menu_bar.addMenu("File")
-        exit_action = file_menu.addAction("Exit")
-        exit_action.triggered.connect(self.close)
-
-        # Create dock widgets for each monitor
-        self.monitor_widgets: List[MonitorWidget] = []
-        for key, monitor in self.health_service.monitors.items():
-            monitor_widget = MonitorWidget(monitor)
-            self.monitor_widgets.append(monitor_widget)
-            monitor_scroll = ScrollWidget()
-            monitor_scroll.addWidget(monitor_widget)
-
-            position = monitor.dock.lower()
-            if position == "center":
-                self.add_document(monitor_scroll, monitor.name)
-                action = self.create_tab_toggle_action(monitor.name, monitor_scroll)
-                view_menu.addAction(action)
-                self._view_actions[key] = action
-                continue
-
-            dock = QDockWidget(monitor.name, self)
-            dock.setWidget(monitor_scroll)
-            dock.setObjectName(f"{key}DockWidget")
-            dock.setFeatures(
-                QDockWidget.DockWidgetFeature.DockWidgetMovable
-                | QDockWidget.DockWidgetFeature.DockWidgetClosable
-                | QDockWidget.DockWidgetFeature.DockWidgetFloatable
-            )
-            action = dock.toggleViewAction()
-            view_menu.addAction(action)
-            self._view_actions[key] = action
-
-            # Place dock based on monitor.dock
-            if position == "left":
-                self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock)
-            elif position == "right":
-                self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, dock)
-            elif position == "top":
-                self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, dock)
-            elif position == "bottom":
-                self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, dock)
-
-        status_bar = QStatusBar()
-        self.setStatusBar(status_bar)
-        separator = QFrame()
-
-        self._heartbeat_widgets: List[HeartbeatWidget] = []
-        hb_items = list(self.health_service.heartbeats.items())
-        for idx, (key, heartbeat) in enumerate(hb_items):
-            heartbeat_widget = HeartbeatWidget(heartbeat)
-            self._mqtt_service.connect_fail_signal.connect(
-                lambda: heartbeat_widget.reset()
-            )
-            self._mqtt_service.disconnect_signal.connect(
-                lambda: heartbeat_widget.reset()
-            )
-            self._heartbeat_widgets.append(heartbeat_widget)
-            status_bar.addWidget(heartbeat_widget)
-
-            # Separator, only between widgets
-            if idx < len(hb_items) - 1:
-                separator = QFrame()
-                separator.setFrameShape(QFrame.Shape.VLine)
-                separator.setFrameShadow(QFrame.Shadow.Plain)
-                status_bar.addWidget(separator)
-
-        self._wdlms_widget = WdlmsWidget(self.health_service.wdlms)
-        wdlm_scroll = ScrollWidget()
-        wdlm_scroll.addWidget(self._wdlms_widget)
-        dock = QDockWidget(self.health_service.wdlms.name, self)
-        dock.setWidget(wdlm_scroll)
-        dock.setObjectName("wdlmDockWidget")
-        dock.setFeatures(
-            QDockWidget.DockWidgetFeature.DockWidgetMovable
-            | QDockWidget.DockWidgetFeature.DockWidgetClosable
-            | QDockWidget.DockWidgetFeature.DockWidgetFloatable
-        )
-        action = dock.toggleViewAction()
-        view_menu.addAction(action)
-        self._view_actions["wdlm"] = action
-        # Place dock based on monitor.dock
-        if position == "left":
-            self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock)
-        elif position == "right":
-            self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, dock)
-        elif position == "top":
-            self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, dock)
-        elif position == "bottom":
-            self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, dock)
-
-        # Create toolbar to monitor mqtt status
-        mqtt_widget = MqttWidget(self._mqtt_service)
-
-        tool_bar = QToolBar()
-        tool_bar.setObjectName("mqttToolBar")
-        tool_bar.addWidget(mqtt_widget)
-        self.addToolBar(tool_bar)
+    def add_document(self, widget: QWidget, title: str):
+        """Add a new document tab"""
+        index = self.document_tabs.addTab(widget, title)
+        self.document_tabs.setCurrentIndex(index)
+        return index
 
     def close_tab(self, index: int):
         """Handle tab close requests"""
@@ -191,11 +128,106 @@ class MainWindow(QMainWindow):
         action.toggled.connect(toggled)
         return action
 
-    def add_document(self, widget: QWidget, title: str):
-        """Add a new document tab"""
-        index = self.document_tabs.addTab(widget, title)
-        self.document_tabs.setCurrentIndex(index)
-        return index
+    # ========================
+    # Health Service Widgets
+    # ========================
+
+    def _init_monitors(self):
+        # Create dock widgets for each monitor
+        self.monitor_widgets: List[MonitorWidget] = []
+        for key, monitor in self.health_service.monitors.items():
+            monitor_widget = MonitorWidget(monitor)
+            self.monitor_widgets.append(monitor_widget)
+            monitor_scroll = ScrollWidget()
+            monitor_scroll.addWidget(monitor_widget)
+
+            position = monitor.dock.lower()
+            if position == "center":
+                self.add_document(monitor_scroll, monitor.name)
+                action = self.create_tab_toggle_action(monitor.name, monitor_scroll)
+                self.view_menu.addAction(action)
+                self._view_actions[key] = action
+                continue
+
+            dock = QDockWidget(monitor.name, self)
+            dock.setWidget(monitor_scroll)
+            dock.setObjectName(f"{key}DockWidget")
+            dock.setFeatures(
+                QDockWidget.DockWidgetFeature.DockWidgetMovable
+                | QDockWidget.DockWidgetFeature.DockWidgetClosable
+                | QDockWidget.DockWidgetFeature.DockWidgetFloatable
+            )
+            action = dock.toggleViewAction()
+            self.view_menu.addAction(action)
+            self._view_actions[key] = action
+
+            # Place dock based on monitor.dock
+            if position == "left":
+                self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock)
+            elif position == "right":
+                self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, dock)
+            elif position == "top":
+                self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, dock)
+            elif position == "bottom":
+                self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, dock)
+
+    def _init_heartbeats(self):
+        self._heartbeat_widgets: List[HeartbeatWidget] = []
+        hb_items = list(self.health_service.heartbeats.items())
+        separator = QFrame()
+
+        for idx, (key, heartbeat) in enumerate(hb_items):
+            heartbeat_widget = HeartbeatWidget(heartbeat)
+            self._mqtt_service.connect_fail_signal.connect(
+                lambda: heartbeat_widget.reset()
+            )
+            self._mqtt_service.disconnect_signal.connect(
+                lambda: heartbeat_widget.reset()
+            )
+            self._heartbeat_widgets.append(heartbeat_widget)
+            self.status.addWidget(heartbeat_widget)
+
+            # Separator, only between widgets
+            if idx < len(hb_items) - 1:
+                separator = QFrame()
+                separator.setFrameShape(QFrame.Shape.VLine)
+                separator.setFrameShadow(QFrame.Shadow.Plain)
+                self.status.addWidget(separator)
+
+    def _init_wdlms(self):
+        self._wdlms_widget = WdlmsWidget(self.health_service.wdlms)
+        position = self.health_service.wdlms.dock.lower()
+        wdlm_scroll = ScrollWidget()
+        wdlm_scroll.addWidget(self._wdlms_widget)
+        dock = QDockWidget(self.health_service.wdlms.name, self)
+        dock.setWidget(wdlm_scroll)
+        dock.setObjectName("wdlmDockWidget")
+        dock.setFeatures(
+            QDockWidget.DockWidgetFeature.DockWidgetMovable
+            | QDockWidget.DockWidgetFeature.DockWidgetClosable
+            | QDockWidget.DockWidgetFeature.DockWidgetFloatable
+        )
+        action = dock.toggleViewAction()
+        self.view_menu.addAction(action)
+        self._view_actions["wdlm"] = action
+        # Place dock based on monitor.dock
+        if position == "left":
+            self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock)
+        elif position == "right":
+            self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, dock)
+        elif position == "top":
+            self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, dock)
+        elif position == "bottom":
+            self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, dock)
+
+    # ========================
+    # MQTT Service Widgets
+    # ========================
+
+    def _init_mqtt(self):
+        # Create toolbar to monitor mqtt status
+        mqtt_widget = MqttWidget(self._mqtt_service)
+        self.tool.addWidget(mqtt_widget)
 
     # ========================
     # Handlers
